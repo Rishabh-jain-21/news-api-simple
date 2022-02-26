deb32c4a9cd334c0846d6f98dda9ad621c3ffe79
Rishabh Jain <79372016+Rishabh-jain-21@users.noreply.github.com>
2022-02-26 13:30:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("news connected");
 let newsContent = document.getElementById('news-content');
-let apiKey = 'e8ab547cb58b40da83db20f5b30a8c56';
+// let apiKey = 'e8ab547cb58b40da83db20f5b30a8c56';
 let newHtml = "";
 let defaultURL = "./defaultimage.jpg";
 let page = 1;
@@ -9,24 +9,21 @@ let nextButton = document.getElementById('next');
 
 //creating a get request
 let xhr = new XMLHttpRequest();
-xhr.open('GET', `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}&page=${page}&pageSize=100`, true);
+xhr.open('GET', `https://india-today-api.rishabhjain48.repl.co/api/india`, true);
 
 xhr.onload = function () {
     if (this.status === 200) {
         let json = JSON.parse(this.responseText);
-        let articles = json.articles;
-
-        totalResults = json.totalResults;
-        console.log(totalResults);
-        articles.forEach(element => {
+        console.log(json);
+        json.forEach(element => {
             let contentHTML = `<div class="news-container">
             <div class="pic"><img
-                    src="${element.urlToImage != null ? element.urlToImage : defaultURL}"
+                    src="${element.img != null ? element.img : defaultURL}"
                     alt="image"></div>
             <div class="content">
                 <h3>${element.title}</h3>
-                <p>${element.description}</p>
-                <a href="${element.url}">Read more....</a>
+                <p>${element.desp}</p>
+                <a href="https://www.indiatoday.in${element.link}">Read more....</a>
             </div>
 
         </div>`;
